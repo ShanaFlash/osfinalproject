c84eec63c9f93fc1b3b4f509d6c83f6cc05a81e3
Jenifer Nguyen <jwinn19464@gmail.com>
2023-11-22 12:04:28 -0500

    Working multithreaded Google News Scraper

---
+++ project.py
@@ -0,0 +1,40 @@
+import multiprocessing
+from bs4 import BeautifulSoup
+from queue import Queue, Empty
+from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urljoin, urlparse
+import threading
+import requests
+
+# Function to scrape headlines from Google News
+def scrape_google_news():
+    url = 'https://news.google.com/'
+    headers = {
+        'User-Agent': 'Your User Agent String'  # Update with your User Agent
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # Example: Scraping headlines
+        headlines = soup.find_all('h4', {'class': 'gPFEn'})
+        news_headlines = [headline.get_text() for headline in headlines]
+        print("Headlines:")
+        print(news_headlines)
+    else:
+        print("Failed to fetch data from Google News")
+
+# Define the number of threads (pages) to scrape
+num_threads = 5
+
+# Create and start threads
+threads = []
+for _ in range(num_threads):
+    thread = threading.Thread(target=scrape_google_news)
+    threads.append(thread)
+    thread.start()
+
+# Wait for all threads to complete
+for thread in threads:
+    thread.join()
+
+print("All threads completed.")
